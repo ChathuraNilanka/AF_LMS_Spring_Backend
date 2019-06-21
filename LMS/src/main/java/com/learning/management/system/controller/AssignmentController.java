e1b099158b14c28477f557e45da2d5b09f957d2e
unknown <lankadealwis97@gmail.com>
2019-06-21 10:10:43 +0530

    remove button Added

--- LMS/src/main/java/com/learning/management/system/controller/AssignmentController.java
+++ LMS/src/main/java/com/learning/management/system/controller/AssignmentController.java
@@ -6,8 +6,12 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.management.system.model.Assignment;
+import com.learning.management.system.model.Course;
 import com.learning.management.system.repo.AssignmentReposirory;
 
 @RestController
@@ -34,9 +39,13 @@ public class AssignmentController {
 		return assignmentRepo.save(assignment);
     }
 	
-	@GetMapping("/all")
-    public String getAll() {
-        return "wada";
-    }
+//	@GetMapping("/{insId}")
+//    public List<Assignment> getAllByInsructorId( @PathVariable String insId ) {
+//        //return assignmentRepo.findByinstructorIdt(insId);
+//        
+//        Query query = new Query();
+//        query.addCriteria(Criteria.where("instructorId").is("insId"));
+//        return assignmentRepo.findAll(query);
+//    }
 
 }
